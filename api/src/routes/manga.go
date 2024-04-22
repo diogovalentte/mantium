5f9a1014c4afcd006b9b76e870897fcaa36c3ff4
Diogo Valentim <diogovalentte10@gmail.com>
2024-04-22 02:30:36 -0300

    Update API docs of the /v1/mangas/add_to_kaizoku route, add warning of heavy operation

--- api/src/routes/manga.go
+++ api/src/routes/manga.go
@@ -820,7 +820,7 @@ func UpdateMangasMetadata(c *gin.Context) {
 }
 
 // @Summary Add mangas to Kaizoku
-// @Description Add the mangas in the database to Kaizoku. If it fails to add a manga, it will continue with the next manga.
+// @Description Add the mangas in the database to Kaizoku. If it fails to add a manga, it will continue with the next manga. This is a heavy operation depending on the number of mangas in the database.
 // @Produce json
 // @Param status query []int false "Filter which mangas to add by status. 1=reading, 2=completed, 3=on hold, 4=dropped, 5=plan to read. Example: status=1,2,3,5" Example(1,2,3,5)
 // @Success 200 {object} responseMessage
