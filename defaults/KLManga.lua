e4dcfef8c443bbd5dcc957304914f4298b8e7d5b
Diogo Valentim <diogo@valentium.com.br>
2025-10-14 14:39:01 -0300

    fix: set klmanga TLD to ist

--- defaults/KLManga.lua
+++ defaults/KLManga.lua
@@ -15,7 +15,7 @@ HttpUtil = require("http_util")
 ----- VARIABLES -----
 Debug = false
 Client = Http.client({ timeout = 20, insecure_ssl = true, debug = Debug })
-Base = "https://klmanga.lt"
+Base = "https://klmanga.ist"
 --- END VARIABLES ---
 
 ----- MAIN -----
