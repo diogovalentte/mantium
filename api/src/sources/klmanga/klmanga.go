3f84b05e85e61502ff611712987d47634cb8d837
Diogo Valentim <diogo@valentium.com.br>
2026-04-25 14:11:08 -0300

    fix: KLManga TLD

--- api/src/sources/klmanga/klmanga.go
+++ api/src/sources/klmanga/klmanga.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-var baseSiteURL = "https://klmanga.bid"
+var baseSiteURL = "https://klmanga.you"
 
 // Source is the struct for the KLManga source
 type Source struct{}
