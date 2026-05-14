ecc892d42474a6af241c77a74d66e45e6ba0e0b0
Diogo Valentim <diogo@valentium.com.br>
2026-05-14 19:45:39 -0300

    fix: KLManga TLD

--- api/src/sources/klmanga/klmanga.go
+++ api/src/sources/klmanga/klmanga.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-var baseSiteURL = "https://klmanga.you"
+var baseSiteURL = "https://klmanga.click"
 
 // Source is the struct for the KLManga source
 type Source struct{}
--- defaults/KLManga.lua
+++ defaults/KLManga.lua
@@ -15,7 +15,7 @@ HttpUtil = require("http_util")
 ----- VARIABLES -----
 Debug = false
 Client = Http.client({ timeout = 20, insecure_ssl = true, debug = Debug })
-Base = "https://klmanga.you"
+Base = "https://klmanga.click"
 --- END VARIABLES ---
 
 ----- MAIN -----
